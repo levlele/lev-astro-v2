6a7f25d025e55017e8ce9d089b1990775c7ff2aa
Leandro Vilanova <leandrovilanova@gmail.com>
2023-06-21 19:20:24 -0300

    Update jobs anchor style

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -53,31 +53,31 @@ export const ui = {
     'jobs': [
       {
         "job": "Senior Frontend Developer",
-        "place": "<a class='underline hover:bg-secondary bg-primary text-black transition-colors ease-in-out duration-500' href='https://www.univtec.com/' rel='noopener noreferrer' target='_blank'>Univ</a>",
+        "place": "<a class='underline hover:bg-secondary transition-colors ease-in-out duration-500' href='https://www.univtec.com/' rel='noopener noreferrer' target='_blank'>Univ</a>",
         "years": "2022 - 2023",
         "info": "Design and development of reusable components for libraries in React, React Native, and Vue, in close collaboration with the development team, for use in streaming platforms. Maintenance and updating of existing components."
       },
       {
         "job": "Frontend Developer",
-        "place": "<a class='underline hover:bg-secondary bg-primary text-black transition-colors ease-in-out duration-500' href='https://www.workana.com/' rel='noopener noreferrer' target='_blank'>Workana</a>",
+        "place": "<a class='underline hover:bg-secondary transition-colors ease-in-out duration-500' href='https://www.workana.com/' rel='noopener noreferrer' target='_blank'>Workana</a>",
         "years": "2021 - 2022",
         "info": "Together with the UX team, we carried out a complete rebranding of the platform throughout the first quarter of 2022. We worked on redesigning and implementing the home page, as well as the onboarding process for new talents."
       },
       {
         "job": "Frontend Developer",
-        "place": "Insite Latin America S.A. para <a class='underline hover:bg-secondary bg-primary text-black transition-colors ease-in-out duration-500' href='https://education.hpe.com/ww/en/training/index.html' rel='noopener noreferrer' target='_blank'>Hewlett Packard Education</a>",
+        "place": "Insite Latin America S.A. para <a class='underline hover:bg-secondary transition-colors ease-in-out duration-500' href='https://education.hpe.com/ww/en/training/index.html' rel='noopener noreferrer' target='_blank'>Hewlett Packard Education</a>",
         "years": "2020 - 2021",
         "info": "Maintenance and updating of the HPE Education Services website. Together with 2 developers, we completely refactored the site, componentizing it with React.js."
       },
       {
         "job": "Frontend Developer",
-        "place": "<a class='underline hover:bg-secondary bg-primary text-black transition-colors ease-in-out duration-500' href='https://insite-la.com/en/index.html' rel='noopener noreferrer' target='_blank'>Insite Latin America</a> para <span class='md:whitespace-nowrap'>Mercado Libre</span>",
+        "place": "<a class='underline hover:bg-secondary transition-colors ease-in-out duration-500' href='https://insite-la.com/en/index.html' rel='noopener noreferrer' target='_blank'>Insite Latin America</a> para <span class='md:whitespace-nowrap'>Mercado Libre</span>",
         "years": "2020",
         "info": "Active member of the New Buyers Activation Beta Squad. Working in collaboration with the UX, marketing, and McKinsey & Company consulting teams, in the development of experiments for both email marketing and landing pages."
       },
       {
         "job": "Project Manager / Frontend Developer",
-        "place": "<a class='underline hover:bg-secondary bg-primary text-black transition-colors ease-in-out duration-500' href='https://insite-la.com/en/index.html' rel='noopener noreferrer' target='_blank'>Insite Latin America</a> para <span class='md:whitespace-nowrap'>Mercado Pago</span>",
+        "place": "<a class='underline hover:bg-secondary transition-colors ease-in-out duration-500' href='https://insite-la.com/en/index.html' rel='noopener noreferrer' target='_blank'>Insite Latin America</a> para <span class='md:whitespace-nowrap'>Mercado Pago</span>",
         "years": "2015 - 2020",
         "info": "In charge of coordinating and structuring projects for the account, as well as controlling and allocating resources. Responsible for implementing email marketing campaigns.<br> Development of an internal email generator using Nunjucks and Gulp to compile on-site developed modules and components, streamlining the process of customization and creation of on-demand templates.<br><br> Development and maintenance of landing pages.<br> In charge of developing the main marketing microsite for the account, as well as promotional campaigns."
       },
@@ -142,19 +142,19 @@ export const ui = {
     'jobs': [
       {
         "job": "Senior Frontend Developer",
-        "place": "<a class='underline hover:bg-secondary hover:text-black transition-colors ease-in-out duration-500' href='https://www.univtec.com/' rel='noopener noreferrer' target='_blank'>Univ</a>",
+        "place": "<a class='underline hover:bg-secondary transition-colors ease-in-out duration-500' href='https://www.univtec.com/' rel='noopener noreferrer' target='_blank'>Univ</a>",
         "years": "2022 - 2023",
         "info": "Diseño y desarrollo de componentes reutilizables para librerías en React, React Native y Vue, en colaboración estrecha con el equipo de desarrollo, para su utilización en plataformas de streaming. Mantenimiento y actualización de componentes existentes."
       },
       {
         "job": "Frontend Developer",
-        "place": "<a class='underline hover:bg-secondary hover:text-black transition-colors ease-in-out duration-500' href='https://www.workana.com/' rel='noopener noreferrer' target='_blank'>Workana</a>",
+        "place": "<a class='underline hover:bg-secondary transition-colors ease-in-out duration-500' href='https://www.workana.com/' rel='noopener noreferrer' target='_blank'>Workana</a>",
         "years": "2021 - 2022",
         "info": "En conjunto con el equipo de UX realizamos un rebranding completo de la plataforma a lo largo del primer Q del 2022. Se trabajó en el rediseño e implementación de la home page así como también en el proceso de onboarding de nuevos talentos."
       },
       {
         "job": "Frontend Developer",
-        "place": "Insite Latin America S.A. para <a class='underline hover:bg-secondary hover:text-black transition-colors ease-in-out duration-500' href='https://education.hpe.com/ww/en/training/index.html' rel='noopener noreferrer' target='_blank'>Hewlett Packard Education</a>",
+        "place": "Insite Latin America S.A. para <a class='underline hover:bg-secondary transition-colors ease-in-out duration-500' href='https://education.hpe.com/ww/en/training/index.html' rel='noopener noreferrer' target='_blank'>Hewlett Packard Education</a>",
         "years": "2020 - 2021",
         "info": "Mantenimiento y actualización del sitio de HPE Education Services. En conjunto con 2 desarrolladores refactorizamos por completo el sitio, componetizándolo con React.js."
       },
