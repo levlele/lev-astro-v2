08885cbfe6fd1db3f3e92b3e38bb1a18e5676dca
Leandro Vilanova <leandro.vilanova@univtec.com>
2024-03-11 02:51:38 -0300

    Fix insite url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+// import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-	trailingSlash: "always",
-	output: 'static',
-	experimental: {
-		assets: true,
-	},
+  output: "static",
+  experimental: {
+    assets: true,
+  },
   build: {
     format: "directory",
   },
-	integrations: [
-	mdx(),
-	tailwind(),
-	],
+  integrations: [tailwind()],
 });
---
+++ src/data/books.js
@@ -0,0 +1,38 @@
+export const books = [
+  {
+    id: "clean-code",
+    title: "Clean Code",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg",
+    opinion: "Lala",
+    author: "Robert C. Martin",
+    excerpt:
+      "Even bad code can function. But if code isn’t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn’t have to be that way.",
+  },
+  {
+    id: "the-clean-coder",
+    title: "The Clean Coder: A Code of Conduct for Professional Programmers",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg",
+    opinion: "",
+    author: "Robert C. Martin",
+    excerpt:
+      "Programmers who endure and succeed amidst swirling uncertainty and nonstop pressure share a common attribute: They care deeply about the practice of creating software. They treat it as a craft. They are professionals.",
+  },
+  {
+    id: "javascript-the-good-parts",
+    title: "JavaScript: The Good Parts",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg",
+    opinion: "",
+    author: "Douglas Crockford",
+    excerpt:
+      "Most programming languages contain good and bad parts, but JavaScript has more than its share of the bad, having been developed and released in a hurry before it could be refined. This authoritative book scrapes away these bad features to reveal a subset of JavaScript that's more reliable, readable, and maintainable than the language as a whole—a subset you can use to create truly extensible and efficient code.",
+  },
+  {
+    id: "eloquent-javascript",
+    title: "Eloquent JavaScript: A Modern Introduction to Programming",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg",
+    opinion: "",
+    author: "Marijn Haverbeke",
+    excerpt:
+      "JavaScript lies at the heart of almost every modern web application, from social apps to the newest browser-based games. Though simple for beginners to pick up and play with, JavaScript is a flexible, complex language that you can use to build full-scale applications.",
+  },
+];
